e739635f64284f02ba9859008dcd696a431cf1b0
JR Dev <astronomi@gmail.com>
2020-02-19 17:19:41 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
-import history from './services/history.js';
+import history from './services/history';
 import Routes from './routes'
 
 import GlobalStyle from './styles/global';
+
 
 const App: React.FC = () => {
   return (
